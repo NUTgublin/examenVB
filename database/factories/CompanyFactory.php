5c6444717aa1b6f1b4f3e3949c2b2c2d18aec134
Khalil <ps250618@summacollege.nl>
2026-09-18 13:03:18 +0200

    eerste commit

---
+++ database/factories/CompanyFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Company;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Company>
+ */
+class CompanyFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'name' => fake()->company(),
+            'address' => fake()->address(),
+            'email' => fake()->unique()->companyEmail(),
+            'postal_code' => fake()->postcode(),
+            'phone_number' => fake()->phoneNumber(),
+        ];
+    }
+}
